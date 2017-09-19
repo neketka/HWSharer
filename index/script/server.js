53080910be26b0efd4f093a89c1c0f4600fd86d2
neketka <neketka@gmail.com>
2017-09-18 22:23:12 -0400

    Fixed address

--- index/script/server.js
+++ index/script/server.js
@@ -3,7 +3,7 @@ var memberType = "none"; //none : admin : member
 
 function postServer(command, data, success, fail=0)
 {
-    var call = $.ajax({type: "POST", url: ("http://hwsharer-hwsharer.7e14.starter-us-west-2.openshiftapps.com/:8080" + command), timeout: 4000, 
+    var call = $.ajax({type: "POST", url: ("http://hwsharer-hwsharer.7e14.starter-us-west-2.openshiftapps.com" + command), timeout: 4000, 
         data: data, success: success });
     if (fail)
         call.fail(success);
